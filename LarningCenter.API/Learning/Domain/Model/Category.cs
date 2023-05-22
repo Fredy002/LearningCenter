92cd02841f916427a9c68a53b0e85b810d798254
Fredy Antonio Almeyda Alania <totoal0520@gmail.com>
2023-05-22 11:54:30 -0500

    Generate Base Response

---
+++ LarningCenter.API/Learning/Domain/Model/Category.cs
@@ -0,0 +1,11 @@
+namespace LarningCenter.API.Learning.Domain.Model;
+
+public class Category
+{
+   public int Id { get; set; }
+   public string Name { get; set; }
+   
+   // Relationships
+
+   public IList<Tutorial> Tutorials { get; set; } = new List<Tutorial>();
+}
