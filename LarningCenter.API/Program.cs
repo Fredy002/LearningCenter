2479a63d82f092b3c6c427414dfe5d24ceee3c60
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2023-05-28 23:33:22 -0500

    DataBase and Interface configuration

--- LarningCenter.API/Program.cs
+++ LarningCenter.API/Program.cs
@@ -1,11 +1,44 @@
+using LarningCenter.API.Learning.Domain.Repositories;
+using LarningCenter.API.Learning.Domain.Services;
+using LarningCenter.API.Learning.Mapping;
+using LarningCenter.API.Learning.Persistence.Repositories;
+using LarningCenter.API.Learning.Services;
+using LarningCenter.API.Shared.Persistence.Contexts;
+using LarningCenter.API.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Database Context Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(
+        options => options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors()
+);
+
+// Add lowerCase routes
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// AutoMapper Configuration
+builder.Services.AddAutoMapper(
+    typeof(ModelToResourceProfile),
+    typeof(ResourceToModelProfile)
+);
+
+// Dependency Injection Configuration
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
@@ -14,6 +47,13 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+// Validation for ensuring Database Objects are created
+using (var scope = app.Services.CreateScope())
+using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+{
+    context.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
